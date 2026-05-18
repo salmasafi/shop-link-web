85acefed72293b60c3c4d360a1db0f5a1c1ece3b
salmasafi <salmasafi126@gmail.com>
2026-05-18 20:11:01 +0300

    deploy: Flutter web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2686841321" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2507526879" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
