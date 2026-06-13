6b46fdbde6aaeede57e5b05c5376d6aa0d013161
Salma <salmasafi126@gmail.com>
2026-06-14 01:55:32 +0300

    Update web build from multi_tenants_e_commerce

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3151579543"
+    serviceWorkerVersion: "69479979"
   }
 });
